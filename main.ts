fcf966a9d91613e8c15857057c58ad55682d416b
Jboucly <julien.boucly@apini.fr>
2022-09-20 15:47:57 +0200

    feat: init ts config

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = require("fs");
-const path_1 = require("path");
-const http_1 = require("http");
+import { readFile } from "fs";
+import { join, extname } from "path";
+import { createServer } from "http";
+
 const port = 3000;
 const hostname = 'localhost';
 const mimeTypeList = {
@@ -22,32 +21,41 @@ const mimeTypeList = {
     '.otf': 'application/font-otf',
     '.wasm': 'application/wasm'
 };
-(0, http_1.createServer)((request, response) => {
+
+createServer((request, response) => {
     console.log('[REQUEST URL] : ', request.url);
+
     var filePath = `.${request.url}`;
+
     if (filePath === './') {
-        filePath = (0, path_1.join)(__dirname, 'views', 'index.html');
+        filePath = join(__dirname, 'views', 'index.html');
     }
-    const extensionName = String((0, path_1.extname)(filePath)).toLowerCase();
-    const contentType = mimeTypeList[extensionName] || 'application/octet-stream';
-    (0, fs_1.readFile)(filePath, (error, content) => {
+
+    const extensionName = String(extname(filePath)).toLowerCase();
+    const contentType = (mimeTypeList as unknown as string)[extensionName as any] || 'application/octet-stream';
+
+    readFile(filePath, (error, content) => {
         if (error) {
             console.error('[ERROR] : ', error);
+
             if (error.code === 'ENOENT') {
-                (0, fs_1.readFile)((0, path_1.join)(__dirname, 'views', '404.html'), (error, content) => {
-                    response.writeHead(404, { 'Content-Type': 'text/html' });
-                    response.end(content, 'utf-8');
-                });
-            }
-            else {
+                readFile(
+                    join(__dirname, 'views', '404.html'),
+                    (error, content) => {
+                        response.writeHead(404, { 'Content-Type': 'text/html' });
+                        response.end(content, 'utf-8');
+                    }
+                );
+            } else {
                 response.writeHead(500);
                 response.end(`Sorry, check with the site admin for error: ${error.code} \n`);
             }
-        }
-        else {
+        } else {
             response.writeHead(200, { 'Content-Type': contentType });
             response.end(content, 'utf-8');
         }
     });
+
 }).listen(port);
+
 console.info(`Server running at http://${hostname}:${port}/`);
